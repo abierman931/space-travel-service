ce0689433169f26eb577d5be5e87fb870200acd0
abierman931 <alexandra.bierman18@gmail.com>
2026-09-18 12:59:05 +0200

    Design task

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,19 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+// Run this code when a form is submitted to 'destination-answer'
+router.post('/destination-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'travelDestination'
+  var travelDestination = req.session.data['travelDestination']
+
+  // Check whether the variable matches a condition
+  if (travelDestination == "The Moon"){
+    // Send user to next page
+    res.redirect('/the-moon/name')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/mars/name')
+  }
+
+})
